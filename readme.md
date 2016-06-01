a974c4181fa36df02fdb01eb3012eeac954df91b
Roger Cruz <rbaltazarc@gmail.com>
2016-06-01 17:29:25 -0500

    Solución de dependencias

---
+++ readme.md
@@ -0,0 +1,10 @@
+## Installation
+
+1. Clone repo
+2. run 'npm install'
+2. run 'bower install'
+
+## Usage
+
+Preview
+'ionic serve --lab'
--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'ionic-todo' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('ionic-todo', ['ionic','ionic.service.core', 'LocalStorageModule']);
+var app = angular.module('ionic-todo', ['ionic', 'LocalStorageModule']);
 
 app.run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
